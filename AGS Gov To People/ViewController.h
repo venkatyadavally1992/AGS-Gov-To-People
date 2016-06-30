a0d3198a4408e5289c8d65fc80b3741c6cef6710
venkat <venkat@venkats-mac.local>
2016-06-30 11:42:03 -0700

    Initial Commit

---
+++ AGS Gov To People/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGS Gov To People
+//
+//  Created by venkat on 6/30/16.
+//  Copyright © 2016 AGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AGS Gov To People/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AGS Gov To People
+//
+//  Created by venkat on 6/30/16.
+//  Copyright © 2016 AGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
